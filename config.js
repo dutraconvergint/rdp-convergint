a3b4414635ca6301c7b8a02bad6d8c674400eadc
dutraconvergint <tiago.convergint@gmail.com>
2026-05-29 14:41:41 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,14 @@
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCpaZuife0p6M2bvwyllnf5TtuB93SuQTk",
+  authDomain: "rdp-convergint.firebaseapp.com",
+  projectId: "rdp-convergint",
+  storageBucket: "rdp-convergint.firebasestorage.app",
+  messagingSenderId: "851747330602",
+  appId: "1:851747330602:web:3479e8d4fb2210a6abc153",
+  measurementId: "G-2CGYD3KK5N"
+};
